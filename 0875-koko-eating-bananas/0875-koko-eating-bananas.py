10771fd923783358d60d43ff3d3b57881d0ab1ad
Mehul vishwakarma <mehulvinodv@gmail.com>
2026-07-02 23:56:52 +0530

    Time: 169 ms (52.65%), Space: 20.5 MB (79.88%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -1,21 +1,19 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        def fun(piles,speed):
+        def fun(speed):#condition function
             time=0
             for pile in piles:
-                time=time+pile//speed
-                if pile%speed!=0:
-                    time+=1
+                time=time+(pile+speed-1)//speed
             return time
         k=-1#bananas-per-hour eating speed$0
         l=1#lowest speed of pile
         r=max(piles)#piles[-1] is not always highest unless array is sorted. max(piles) is correct.
         while l<=r:
             mid=(l+r)//2#speed
-            hour=fun(piles,mid)
+            hour=fun(mid)
             if hour>h:
-                l=mid+1
-            else:
+                l=mid+1#❌ Speed too slow. so Need a larger speed,Move Right.
+            else:#hours <= h #speed works butTry finding a smaller speed.
                 k=mid
                 r=mid-1
         return k
